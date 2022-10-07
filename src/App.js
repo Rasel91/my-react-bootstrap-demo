218ba4cf91a8a4ea53406dabfe2c0a8dd6809a71
Rasel91 <srasel.dpe@gmail.com>
2022-10-07 21:52:07 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Button, CardGroup } from 'react-bootstrap';
+import Header from './components/Header/Header';
+import CardGroups from './components/CardGroup/CardGroups';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button> My New Button</Button>
+      <button> My Normal button</button>
+      {/* Import button form other components */}
+      {/* Apply the React Bootstap Please see the Video No:51-2 Install React Bootstrap in your react application */}
+      <Header></Header>
+      <CardGroups></CardGroups>
+    </div>
+  );
+}
+
+export default App;
